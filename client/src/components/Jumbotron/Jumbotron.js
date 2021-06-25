71deb43bba58c72e8e097a8a6a1d181cf281231b
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-24 20:44:32 -0400

    commit

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -5,9 +5,9 @@ function Jumbotron() {
     return (
         <div>
             <div className="jumbotron">
-                <h1 className="display-4 text-center myText">Google Books Search</h1>
+                <h1 className="display-4 text-center myText">Bookworm book Search</h1>
                 <br></br>
-                <h2 className="text-center myText">Search for and save books of interest. Built with React.</h2>
+                <h2 className="text-center myText">Search for awesome books!!</h2>
             </div>
         </div>
     )
